19f00824dea4a0a73dfcdbc5c948965216f6d26a
“LoozerUser” <shaisarfaty007@gmail.com>
2023-05-02 15:16:36 +0300

    a

--- service-worker.js
+++ service-worker.js
@@ -53,7 +53,7 @@ self.addEventListener('fetch', (event) => {
         console.log('Fetch failed; returning offline page instead.', error);
 
         const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
+        const cachedResponse = await cache.addAll([OFFLINE_URL, '/scripts/app.js']);
         return cachedResponse;
       }
     })());
