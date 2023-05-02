2481f68293a37d681774aa2ebaebfa8ffc42f8cd
“LoozerUser” <shaisarfaty007@gmail.com>
2023-05-02 10:12:54 +0300

    a

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,7 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js');
+}
+
 // set up basic variables for app
 
 const record = document.querySelector('.record');
