024a89f0d23471ebdb18f3f2b29a0017a62bc3a1
“LoozerUser” <shaisarfaty007@gmail.com>
2023-05-02 10:23:07 +0300

    a

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,14 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js');
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js").then(
+    (registration) => {
+      console.log("Service worker registration succeeded:", registration);
+    },
+    (error) => {
+      console.error(`Service worker registration failed: ${error}`);
+    }
+  );
+} else {
+  console.error("Service workers are not supported.");
 }
 
 // set up basic variables for app
