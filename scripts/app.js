d53efca61f4defbb8e549e6857386821718aef0e
“LoozerUser” <shaisarfaty007@gmail.com>
2023-05-02 10:25:44 +0300

    a

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/service-worker.js").then(
+  navigator.serviceWorker.register("scripts/service-worker.js").then(
     (registration) => {
       console.log("Service worker registration succeeded:", registration);
     },
